7d68bf1ce717701ff51c2d0f8b404e325d0cf65f
Biybiruza <ibashovabiybi@gmail.com>
2021-07-18 02:38:34 +0500

    korinisler

---
+++ app/src/main/java/com/biybiruza/ajiniyazqosbayull/ui/shigarmalar/ShigarmalarDizimFragment.kt
@@ -0,0 +1,7 @@
+package com.biybiruza.ajiniyazqosbayull.ui.shigarmalar
+
+import androidx.fragment.app.Fragment
+import com.biybiruza.ajiniyazqosbayull.R
+
+class ShigarmalarDizimFragment : Fragment(R.layout.fragment_shigarmalar_dizim) {
+}
